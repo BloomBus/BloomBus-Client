754c4b29e354009c7c6e1094055746a2f07bac8b
John Gibson <dev.johngibson@gmail.com>
2018-10-10 03:23:23 -0400

    React conversion

--- src/App.js
+++ src/App.js
@@ -1,48 +1,14 @@
 import React, { Component } from 'react';
-import { Router, browserHistory, Route, Link } from 'react-router'
-import logo from './logo.svg';
+import { Router, browserHistory, Route } from 'react-router'
 import './App.css';
-
-const Page = ({ title }) => (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>{title}</h2>
-      </div>
-      <p className="App-intro">
-        This is the {title} page.
-      </p>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-      <p>
-        <Link to="/about">About</Link>
-      </p>
-      <p>
-        <Link to="/settings">Settings</Link>
-      </p>
-    </div>
-)
-
-const Home = (props) => (
-  <Page title="Home"/>
-)
-
-const About = (props) => (
-  <Page title="About"/>
-)
-
-const Settings = (props) => (
-  <Page title="Settings"/>
-)
+import 'antd-mobile/dist/antd-mobile.css';
+import Home from './components/Home/Home.jsx';
 
 class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
         <Route path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/settings" component={Settings}/>
       </Router>
     );
   }
