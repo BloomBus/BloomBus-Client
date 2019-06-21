1645b4a516d65c4abbb087254338c5d45528f50d
John Gibson <dev@johngibson.me>
2019-06-20 21:14:39 -0700

    Replace LoopsCarousel w/ a swipeable bottom sheet

--- src/components/LoopsCarousel/LoopsCarousel.jsx
+++ src/components/LoopsCarousel/LoopsCarousel.jsx
@@ -1,49 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
+
+import SwipeableBottomSheet from 'react-swipeable-bottom-sheet';
 import Carousel from 'nuka-carousel';
 import tinycolor from 'tinycolor2';
 import posed from 'react-pose';
 
+import PagingDots from './PagingDots';
 import StopsList from '../StopsList/StopsList';
 import CarouselMode from './CarouselMode.ts';
 import expandPath from '../../images/expand.svg';
 import './LoopsCarousel.css';
 
 // See CarouselMode.ts
-const PosedWrapper = posed.div({
-  0: { // Collapsed
-    height: 110,
-    delayChildren: 200,
-    staggerChildren: 1000,
-    transition: {
-      default: { ease: 'easeOut', duration: 150 },
-    },
-  },
-  1: { // Expanded
-    height: 400,
-    transition: {
-      default: { ease: 'easeOut', duration: 150 },
-    },
-  },
-  2: { // Hidden
-    height: 0,
-    transition: {
-      default: { ease: 'easeOut', duration: 150 },
-    },
-  },
-});
-
 const ModeIcon = posed.img({
   0: {
-    rotate: 0,
+    rotate: 180,
   },
   1: {
     rotate: 90,
   },
   2: {
-    rotate: 180,
+    rotate: 0,
   },
 });
+
+const PagingDotsContainer = ({ node, ...props }) => {
+  if (!node) return null;
+  return createPortal(<PagingDots {...props} />, node);
+};
 
 class LoopsCarousel extends Component {
   constructor(props) {
@@ -52,29 +38,17 @@ class LoopsCarousel extends Component {
       mode: CarouselMode.Collapsed,
     };
 
+    this.pagingDotsContainerRef = React.createRef();
+
     this.onModeButtonClick = this.onModeButtonClick.bind(this);
     this.onLoopClick = this.onLoopClick.bind(this);
+    this.onBottomSheetChange = this.onBottomSheetChange.bind(this);
   }
 
   onModeButtonClick() {
-    let newMode;
-    switch (this.state.mode) {
-      case CarouselMode.Expanded:
-        newMode = CarouselMode.Collapsed;
-        break;
-      case CarouselMode.Collapsed:
-        newMode = CarouselMode.Hidden;
-        break;
-      case CarouselMode.Hidden:
-        newMode = CarouselMode.Collapsed;
-        break;
-      default:
-        newMode = CarouselMode.Collapsed;
-        break;
-    }
-    this.setState({
-      mode: newMode,
-    });
+    this.setState(prevState => ({
+      mode: prevState.mode === CarouselMode.Expanded ? CarouselMode.Collapsed : CarouselMode.Expanded,
+    }));
   }
 
   onLoopClick() {
@@ -84,32 +58,52 @@ class LoopsCarousel extends Component {
     });
   }
 
+  onBottomSheetChange(isOpen) {
+    this.setState({
+      mode: isOpen ? CarouselMode.Expanded : CarouselMode.Collapsed,
+    });
+  }
+
   render() {
     return (
-      <PosedWrapper
-        className="loops-carousel-wrapper"
-        pose={this.state.mode.toString()}
-      >
-        <button
-          type="button"
-          className="mode-btn"
-          onClick={this.onModeButtonClick}
-          aria-label={this.state.mode === CarouselMode.Collapsed ? 'Hide' : 'Show'}
+      <Fragment>
+        <div ref={this.pagingDotsContainerRef} className="paging-dots-container" />
+        <SwipeableBottomSheet
+          overflowHeight={110}
+          marginTop={50}
+          topShadow={false}
+          overlay={false}
+          shadowTip
+          scrollTopAtClose
+          bodyStyle={{
+            backgroundColor: 'none',
+          }}
+          defaultOpen
+          open={this.state.mode === CarouselMode.Expanded}
+          onChange={this.onBottomSheetChange}
         >
-          <ModeIcon src={expandPath} alt="" pose={this.state.mode.toString()} />
-        </button>
-        <Carousel
-          swiping
-          dragging
-          afterSlide={(slideIndex) => { this.props.onSelectedLoopChanged(slideIndex); }}
-          className="carousel"
-          slideWidth={1.0}
-          heightMode="max"
-          easing="easeQuadOut"
-          renderCenterLeftControls={({ previousSlide }) => null}
-          renderCenterRightControls={({ nextSlide }) => null}
-        >
-          {
+          <div className="loops-carousel-wrapper">
+            <button
+              type="button"
+              className="mode-btn"
+              onClick={this.onModeButtonClick}
+              aria-label={this.state.mode === CarouselMode.Collapsed ? 'Hide' : 'Show'}
+            >
+              <ModeIcon src={expandPath} alt="" pose={this.state.mode.toString()} />
+            </button>
+            <Carousel
+              swiping
+              dragging
+              afterSlide={(slideIndex) => { this.props.onSelectedLoopChanged(slideIndex); }}
+              className="carousel"
+              slideWidth={1.0}
+              heightMode="max"
+              easing="easeQuadOut"
+              renderCenterLeftControls={({ previousSlide }) => null}
+              renderCenterRightControls={({ nextSlide }) => null}
+              renderBottomCenterControls={props => <PagingDotsContainer node={this.pagingDotsContainerRef.current} {...props} />}
+            >
+              {
             this.props.loops.map(loop => (
               <div
                 key={loop.properties.name}
@@ -118,21 +112,22 @@ class LoopsCarousel extends Component {
                 tabIndex="0"
                 onClick={this.onLoopClick}
                 onKeyDown={this.onLoopClick}
+                style={{
+                  backgroundImage: `linear-gradient(to right, ${loop.properties.color}, ${tinycolor(loop.properties.color).spin(15).toHexString()})`,
+                }}
               >
-                <div
-                  className={`carousel__card__head ${this.state.mode === CarouselMode.Expanded && 'carousel__card__head--expanded'}`}
-                  style={{
-                    backgroundImage: `linear-gradient(to right, ${loop.properties.color}, ${tinycolor(loop.properties.color).spin(15).toHexString()})`,
-                  }}
-                >
+                <div className="carousel__card__head">
+                  <div className="carousel__card__notch" />
                   <span className="carousel__card__name">{loop.properties.name}</span>
                 </div>
-                {this.state.mode === CarouselMode.Expanded && <StopsList stops={this.props.stops} />}
+                <StopsList stops={this.props.stops} />
               </div>
             ))
           }
-        </Carousel>
-      </PosedWrapper>
+            </Carousel>
+          </div>
+        </SwipeableBottomSheet>
+      </Fragment>
     );
   }
 }
