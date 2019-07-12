bf25480ea826382fd0ab36337856a68097f4611b
John Gibson <j.gibson@esri.com>
2019-07-12 16:08:13 -0700

    Remove google.maps dependencies

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,18 +1,5 @@
 import PropTypes from 'prop-types';
 
-/* global google */
-export const swBound = new google.maps.LatLng({
-  lat: 41.005188,
-  lng: -76.452374,
-});
-
-export const neBound = new google.maps.LatLng({
-  lat: 41.019014,
-  lng: -76.443321,
-});
-
-export const campusBounds = new google.maps.LatLngBounds(swBound, neBound);
-
 export const mapStyles = [{
   featureType: 'poi',
   elementType: 'labels',
@@ -20,11 +7,6 @@ export const mapStyles = [{
     visibility: 'off',
   }],
 }];
-
-export const campusCenter = {
-  lat: -34.397,
-  lng: 150.644,
-};
 
 export const geoJSONFeatureShape = PropTypes.objectOf(
   PropTypes.shape({
