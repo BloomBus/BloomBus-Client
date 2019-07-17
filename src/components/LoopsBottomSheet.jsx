a50ed36ca1dfb268374b0d5c0b20a9f1cc9dc89f
John Gibson <j.gibson@esri.com>
2019-07-17 10:04:21 -0700

    Create LoopsBottomSheet

--- src/components/LoopsBottomSheet.jsx
+++ src/components/LoopsBottomSheet.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import SwipeableBottomSheet from 'react-swipeable-bottom-sheet';
+import geoJSONFeatureShape from '../utils/geoJSONFeatureShape';
 
 const LoopsBottomSheetContainer = styled.div`
   height: 100%;
@@ -10,11 +11,24 @@ const LoopsBottomSheetContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  &::after {
+    width: 26px;
+    height: 4px;
+    border-radius: 2px;
+    display: inline;
+    position: absolute;
+    left: calc(50% - 13px);
+    top: 5px;
+    background-color: #dbdbdb;
+    content: "";
+  }
 `;
 
 const LoopListItem = styled.button`
   display: block;
   width: 100%;
+  height: 4em;
 
   background-color: #ffffff;
   outline: none;
@@ -22,38 +36,44 @@ const LoopListItem = styled.button`
   border-top: 2px solid #f1f1f1;
   padding: 0.85em;
   text-align: left;
+  font-family: "Product Sans";
 `;
 
 const LoopsBottomSheetTitle = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 3em;
+  height: 2.4em;
+  margin-top: 1em;
   text-transform: uppercase;
+  font-family: "Product Sans";
+  font-weight: 600;
 `;
 
 class LoopsBottomSheet extends Component {
-  onLoopClick() {}
-
   render() {
     return (
       <SwipeableBottomSheet
+        open={this.props.open}
+        onChange={this.props.onBottomSheetChange}
         overlay={false}
-        defaultOpen
         topShadow={false}
+        shadowTip={false}
         bodyStyle={{
           borderTopLeftRadius: '1.5rem',
           borderTopRightRadius: '1.5rem',
-          boxShadow: 'rgba(0, 0, 0, 0.157) 0px -4px 5px',
+          boxShadow: this.props.open
+            ? 'rgba(0, 0, 0, 0.157) 0px -4px 5px'
+            : 'none',
         }}
       >
         <LoopsBottomSheetContainer>
-          <LoopsBottomSheetTitle>Current Shuttle Loops</LoopsBottomSheetTitle>
+          <LoopsBottomSheetTitle>Shuttle Loops</LoopsBottomSheetTitle>
           {this.props.loops.map(loop => (
             <LoopListItem
               key={loop.properties.name}
               tabIndex="0"
-              onClick={this.onLoopClick}
+              onClick={() => this.props.onLoopSelect(loop.properties.key)}
             >
               {loop.properties.name}
             </LoopListItem>
@@ -67,7 +87,8 @@ class LoopsBottomSheet extends Component {
 LoopsBottomSheet.defaultProps = {};
 
 LoopsBottomSheet.propTypes = {
-  loops: PropTypes.arrayOf(PropTypes.object).isRequired,
+  loops: PropTypes.arrayOf(geoJSONFeatureShape).isRequired,
+  onLoopSelect: PropTypes.func.isRequired,
 };
 
 export default LoopsBottomSheet;
