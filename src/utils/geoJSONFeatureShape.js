3ff525cf1b4fbb32273f053180f83f57ba06dd2f
John Gibson <j.gibson@esri.com>
2019-07-17 10:12:49 -0700

    Define geoJSONFeatureShape proptype

---
+++ src/utils/geoJSONFeatureShape.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const geoJSONFeatureShape = PropTypes.objectOf(
+  PropTypes.shape({
+    geometry: PropTypes.shape({
+      coordinates: PropTypes.arrayOf(PropTypes.number),
+    }),
+    type: PropTypes.exact('Feature'),
+    properties: PropTypes.shape({
+      timestamp: PropTypes.number,
+    }),
+  }),
+);
+
+export default geoJSONFeatureShape;
