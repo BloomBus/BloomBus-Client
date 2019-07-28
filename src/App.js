9ccac5143df8d9046ceee215eb89831a29fb1a22
Jonathan Gibson <jonathangibson02@gmail.com>
2019-07-28 15:06:22 -0700

    Add LogRocket

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 /* eslint-disable */
 import React, { Component } from 'react';
 import { Router, browserHistory, Route } from 'react-router';
-import './App.css';
 import Home from './components/Home';
 
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
+
+import './App.css';
+
 class App extends Component {
+  componentDidMount() {
+    LogRocket.init('ayfdlv/bloombus');
+    setupLogRocketReact(LogRocket);
+  }
+
   render() {
     return (
       <Router history={browserHistory}>
