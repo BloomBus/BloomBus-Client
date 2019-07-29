a138c47f21af309a9bacae2efbbb5d0490f14c17
Nick Ashenfelter <nashenfelter@gmail.com>
2019-07-28 20:53:05 -0400

    Add Next Stop

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import geoJSONFeatureShape from '../utils/geoJSONFeatureShape';
+
+import ETALabel from './ETALabel';
 
 const SidebarContainer = styled.div`
   box-sizing: border-box;
@@ -63,9 +65,44 @@ const SidebarName = styled.span`
   color: ${props => props.color || 'inherit'};
 `;
 
-class Sidebar extends Component {
+const LoopNextStop = styled.span`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 0.9em;
+  color: rgba(#b5b5b6);
+  margin-top: 0.1em;
+`;
+
+const NextStopSVG = styled.svg`
+  height: 1em;
+  margin-right: 0.2em;
+`;
+
+const NextStopIcon = () => (
+  <NextStopSVG viewBox="0 0 30 20">
+    <path
+      fillRule="evenodd"
+      strokeLinejoin="round"
+      strokeMiterlimit="2"
+      clipRule="evenodd"
+      fill="#777b8e"
+      d="M9.89 10.84h7.31v4.07l6-5.24-6-5.2v4.1H9.88v2.24H0V8.57h9.88a9.7 9.7 0 1 1 0 2.27z"
+    />
+  </NextStopSVG>
+);
+
+class Sidebar extends PureComponent {
   constructor(props) {
     super(props);
+  }
+
+  getRandomStopName(loop) {
+    if (!this.props.loopStops) return '';
+    const { key } = loop.properties;
+    const loopStops = this.props.loopStops[key];
+    const stopKey = loopStops[Math.floor(Math.random() * loopStops.length)];
+    return this.props.stops[stopKey].properties.name;
   }
 
   render() {
@@ -80,6 +117,11 @@ class Sidebar extends Component {
               <SidebarName color={loop.properties.color}>{loop.properties.name}</SidebarName>
             </SidebarHead>
             {/* List Item of Stops */}
+            <LoopNextStop>
+              <NextStopIcon />
+              {this.getRandomStopName(loop)}
+            </LoopNextStop>
+            <ETALabel number={3} />
           </SidebarItem>
         ))}
       </SidebarContainer>
