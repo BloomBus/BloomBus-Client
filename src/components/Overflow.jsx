260e35ae5b6057669a168c65d0c3c7c92e6c6cfd
Nick Ashenfelter <nashenfelter@gmail.com>
2019-08-07 15:28:53 -0400

    Create Overflow.jsx and initial imports

---
+++ src/components/Overflow.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Menu, { MenuItem } from 'calcite-react/Menu';
+
+class Overflow extends Component {
+  render() {
+    return <div>temp</div>;
+  }
+}
+
+export default Overflow;
