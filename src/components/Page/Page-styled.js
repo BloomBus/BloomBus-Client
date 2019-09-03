05fb2d16bb8ff8dc51329cc54b82a7e41b61f189
Jonathan Gibson <jonathangibson02@gmail.com>
2019-09-02 17:54:41 -0700

    Fix Page width styling

--- src/components/Page/Page-styled.js
+++ src/components/Page/Page-styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const PageContainer = styled.div`
 flex: 1;
   display: grid;
-  grid-template-columns: [col1-start] minmax(0, 1fr) [col1-end col2-start] minmax(40em, 100%) [col2-end col3-start] minmax(0, 1fr) [col4-end];
+  grid-template-columns: [col1-start] minmax(0, 1fr) [col1-end col2-start] minmax(40em, minmax(1fr, 100%)) [col2-end col3-start] minmax(0, 1fr) [col4-end];
   background-color: ${props => props.theme.palette.offWhite};
 `;
 
