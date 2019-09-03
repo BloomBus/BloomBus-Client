4cefb15aa4698bca2aaf6d99a3f60a211a7bc441
Jonathan Gibson <jonathangibson02@gmail.com>
2019-09-02 18:20:38 -0700

    Fix Page container scrolling

--- src/components/Page/Page-styled.js
+++ src/components/Page/Page-styled.js
@@ -4,6 +4,7 @@ export const PageContainer = styled.div`
 flex: 1;
   display: grid;
   grid-template-columns: [col1-start] minmax(0, 1fr) [col1-end col2-start] minmax(40em, minmax(1fr, 100%)) [col2-end col3-start] minmax(0, 1fr) [col4-end];
+  overflow-y: scroll;
   background-color: ${props => props.theme.palette.offWhite};
 `;
 
