bdff36604812351903e7789b9d095ddbdf4646a1
Jonathan Gibson <jonathangibson02@gmail.com>
2019-09-03 20:28:24 -0700

    Fix About page column sizing

--- src/components/Page/Page-styled.js
+++ src/components/Page/Page-styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const PageContainer = styled.div`
 flex: 1;
   display: grid;
-  grid-template-columns: [col1-start] minmax(0, 1fr) [col1-end col2-start] minmax(40em, minmax(1fr, 100%)) [col2-end col3-start] minmax(0, 1fr) [col4-end];
+  grid-template-columns: [col1-start] minmax(0, 1fr) [col1-end col2-start] minmax(auto, 60em) [col2-end col3-start] minmax(0, 1fr) [col3-end];
   overflow-y: scroll;
   background-color: ${props => props.theme.palette.offWhite};
 `;
