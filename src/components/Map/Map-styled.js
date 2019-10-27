d294b23c2edeee54a5ac0aeb6f72e29d92b6a0e5
Jonathan Gibson <jonathangibson02@gmail.com>
2019-10-26 22:11:18 -0700

    Fix map controls position

--- src/components/Map/Map-styled.js
+++ src/components/Map/Map-styled.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 import { GeolocateControl, NavigationControl } from 'react-map-gl';
 
-export const StyledGeolocateControl = styled(GeolocateControl)`
+export const MapControlsWrapper = styled.div`
   position: absolute;
-  right: 10px;
   top: 10px;
+  right: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
 
+  & > *:not(:first-child) {
+    margin-top: 0.5rem;
+  }
+`;
+
+export const StyledGeolocateControl = styled(GeolocateControl)`
   button {
     height: 2.4rem;
     width: 2.4rem;
@@ -13,10 +22,6 @@ export const StyledGeolocateControl = styled(GeolocateControl)`
 `;
 
 export const StyledNavigationControl = styled(NavigationControl)`
-  position: absolute;
-  right: 10px;
-  top: 3.6rem;
-  
   button {
     height: 2.4rem;
     width: 2.4rem;
